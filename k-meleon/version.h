6e2ae22e2af7247a037bc0e7aa49a7013aba4f88
binaryc <binaryc@localhost>
2001-02-20 06:12:19 +0000

    Versioning. wee!

---
+++ k-meleon/version.h
@@ -0,0 +1,13 @@
+/*
+**  K-Meleon version information.
+**  This is updated before each build
+*/
+
+#ifdef _DEBUG
+#define VERSION "0.3 Debug"
+#else
+#define VERSION "0.3"
+#endif
+#define BUILD_TIME __TIMESTAMP__
+#define BUILD_NUMBER 133
+#define BUILD_NUMBER_STRING "133"
