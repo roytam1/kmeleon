c65a3a3097a891697b84e20cf72c8af0e1e2e2b4
doozan <doozan@localhost>
2001-04-13 05:11:59 +0000

    Reenabled RegisterBand

--- k-meleon/rebar_menu/rebar_menu.cpp
+++ k-meleon/rebar_menu/rebar_menu.cpp
@@ -109,8 +109,7 @@ void DoRebar(HWND rebarWnd) {
       );
 
    // Register the band name and child hwnd
-   // we don't want them disabling the menu (because you can't get it back!)
-   //kPlugin.kf->RegisterBand(hwndTB, "Menu Bar");
+   kPlugin.kf->RegisterBand(hwndTB, "Menu Bar");
 
    if (!hwndTB){
       MessageBox(NULL, "Failed to create menu toolbar", NULL, 0);
