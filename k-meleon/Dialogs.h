1e9132bc4d240d11dfc5c601d83eb65e82b12cb1
binaryc <binaryc@localhost>
2002-01-11 08:02:47 +0000

    moved dialogs out of components.dll

--- k-meleon/Dialogs.h
+++ k-meleon/Dialogs.h
@@ -50,4 +50,158 @@ protected:
 	virtual void PostNcDestroy();
 };
 
+class CPromptDialog : public CDialog
+{
+public:
+    CPromptDialog(CWnd* pParent, const char* pTitle, const char* pText,
+                  const char* pInitPromptText,
+                  BOOL bHasCheck, const char* pCheckText, int initCheckVal);
+	
+	// Dialog Data
+    enum { IDD = IDD_PROMPT_DIALOG };
+    CString m_csPromptAnswer;
+
+    CString m_csDialogTitle;
+	CString m_csPromptText;
+	BOOL m_bHasCheckBox;
+	CString m_csCheckBoxText;
+	int m_bCheckBoxValue;
+    
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CPromptDialog)
+	protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+	//}}AFX_VIRTUAL
+
+	//{{AFX_MSG(CPromptDialog)
+	virtual BOOL OnInitDialog();
+	//}}AFX_MSG
+    DECLARE_MESSAGE_MAP()
+};
+
+class CPromptPasswordDialog : public CDialog
+{
+public:
+    CPromptPasswordDialog(CWnd* pParent, const char* pTitle, const char* pText,
+                          const char* pInitPasswordText,
+                          BOOL bHasCheck, const char* pCheckText, int initCheckVal);
+	
+	// Dialog Data
+    enum { IDD = IDD_PROMPT_PASSWORD_DIALOG };
+
+    CString m_csDialogTitle;
+	CString m_csPromptText;
+	CString m_csPassword;
+	BOOL m_bHasCheckBox;
+	CString m_csCheckBoxText;
+	int m_bCheckBoxValue;
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CPromptPasswordDialog)		
+	protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+	//}}AFX_VIRTUAL
+
+	//{{AFX_MSG(CPromptPasswordDialog)
+	virtual BOOL OnInitDialog();
+	//}}AFX_MSG
+    DECLARE_MESSAGE_MAP()
+};
+
+class CPromptUsernamePasswordDialog : public CDialog
+{
+public:
+    CPromptUsernamePasswordDialog(CWnd* pParent, const char* pTitle, const char* pText,
+                                  const char* pInitUsername, const char* pInitPassword, 
+		                          BOOL bHasCheck, const char* pCheckText, int initCheckVal);
+	
+	// Dialog Data
+    enum { IDD = IDD_PROMPT_USERPASS_DIALOG };
+
+    CString m_csDialogTitle;
+	CString m_csPromptText;
+	CString m_csUserNameLabel;
+	CString m_csPasswordLabel;
+	CString m_csPassword;
+	CString m_csUserName;
+	BOOL m_bHasCheckBox;
+	CString m_csCheckBoxText;
+	int m_bCheckBoxValue;
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CPromptUsernamePasswordDialog)	
+	protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+	//}}AFX_VIRTUAL
+
+	//{{AFX_MSG(CPromptUsernamePasswordDialog)
+	virtual BOOL OnInitDialog();
+	//}}AFX_MSG
+
+    DECLARE_MESSAGE_MAP()
+};
+
+class CAlertCheckDialog : public CDialog
+{
+public:
+    CAlertCheckDialog(CWnd* pParent, const char* pTitle, const char* pText,
+                  const char* pCheckText, int initCheckVal);
+	
+    // Dialog Data
+    enum { IDD = IDD_ALERT_CHECK_DIALOG };
+
+    CString m_csDialogTitle;
+    CString m_csMsgText;
+    CString m_csCheckBoxText;
+    int m_bCheckBoxValue;
+    
+    // ClassWizard generated virtual function overrides
+    //{{AFX_VIRTUAL(CAlertCheckDialog)
+    protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+    //}}AFX_VIRTUAL
+
+    //{{AFX_MSG(CAlertCheckDialog)
+    virtual BOOL OnInitDialog();
+    //}}AFX_MSG
+
+    DECLARE_MESSAGE_MAP()
+};
+
+class CConfirmCheckDialog : public CDialog
+{
+public:
+    CConfirmCheckDialog(CWnd* pParent, const char* pTitle, const char* pText,
+                  const char* pCheckText, int initCheckVal,
+                  const char *pBtn1Text, const char *pBtn2Text, 
+                  const char *pBtn3Text);
+	
+    // Dialog Data
+    enum { IDD = IDD_CONFIRM_CHECK_DIALOG };
+
+    CString m_csDialogTitle;
+    CString m_csMsgText;
+    CString m_csCheckBoxText;
+    int m_bCheckBoxValue;
+    CString m_csBtn1Text;
+    CString m_csBtn2Text;
+    CString m_csBtn3Text;
+
+    // ClassWizard generated virtual function overrides
+    //{{AFX_VIRTUAL(CConfirmCheckDialog)
+    protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+    //}}AFX_VIRTUAL
+
+    //{{AFX_MSG(CConfirmCheckDialog)
+    virtual BOOL OnInitDialog();
+    afx_msg void OnBtn1Clicked();
+    afx_msg void OnBtn2Clicked();
+    afx_msg void OnBtn3Clicked();
+    //}}AFX_MSG
+
+    DECLARE_MESSAGE_MAP()
+};
+
 #endif //_DIALOG_H_
+
