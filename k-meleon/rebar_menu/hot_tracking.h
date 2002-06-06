8ff18aab0ad22461e3bb9703580af33863575dd3
liffiton <liffiton@localhost>
2002-06-06 23:43:45 +0000

    Cleanup

--- k-meleon/rebar_menu/hot_tracking.h
+++ k-meleon/rebar_menu/hot_tracking.h
@@ -17,7 +17,6 @@
 */
 
 void BeginHotTrack(NMTOOLBAR *nmToolbar, HINSTANCE hInstance, HWND hWnd);
-void myhot(HWND tbwnd, int index, HINSTANCE hInstance, HWND hWnd);
 
 #define SUBMENU_OFFSET 5000 // this is here to distinguish between submenus and menu items, which may have the same id otherwise
 #define WM_DEFERHOTTRACK WM_USER+10 // the message used to defer execution of beginhottrack to let other messages through first
