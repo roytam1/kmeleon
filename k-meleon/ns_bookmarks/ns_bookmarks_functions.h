b7fbbe77054668354df956d4dd59290515df5391
liffiton <liffiton@localhost>
2002-06-30 19:55:55 +0000

    URLs show up in status bar, and I added myself to copyright notices

--- k-meleon/ns_bookmarks/ns_bookmarks_functions.h
+++ k-meleon/ns_bookmarks/ns_bookmarks_functions.h
@@ -1,5 +1,5 @@
 /*
-*  Copyright (C) 2000 Brian Harris
+*  Copyright (C) 2000 Brian Harris, Mark Liffiton
 *
 *  This program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
