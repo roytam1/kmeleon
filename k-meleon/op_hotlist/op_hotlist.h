58b2b48fe687fb707a975d69212c71aba07aa344
ulferikson <ulferikson@localhost>
2002-08-25 08:42:21 +0000

    Initial check-in of the Opera Hotlist plugin.
    The hotlist plugin reads an existing (or creates a new) opera hotlist file. The hotlist can be presented as a menu and as a buttonbar. One can only add new bookmarks to the end, no editing yet.

---
+++ k-meleon/op_hotlist/op_hotlist.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2002 Ulf Erikson <ulferikson@fastmail.fm>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __OP_HOTLIST_H__
+#define __OP_HOTLIST_H__
+
+#ifndef COMPILING_RC
+#include "BookmarkNode.h"
+#include "stdio.h"
+#include "kmeleon_plugin.h"
+#endif
+#include <commctrl.h>
+
+#define IDD_CONFIG                      101
+#define IDC_REBARENABLED                1000
+#define TOOLBAND_NAME "Hotlist"
+#define TOOLBAND_FAILED_TO_CREATE "Failed to create hotlist toolbar"
+#define PLUGIN_NAME "Opera Hotlist Plugin"
+#define MENU_TO_COMMAND(x) (x+SUBMENU_OFFSET)
+
+#define IDB_IMAGES                      158
+
+#define IMAGE_BLANK         -1
+#define IMAGE_FOLDER_CLOSED 0
+#define IMAGE_FOLDER_OPEN   1
+#define IMAGE_BOOKMARK      2
+#define IMAGE_CHEVRON       3
+#define IMAGE_FOLDER_SPECIAL_CLOSED 4
+#define IMAGE_FOLDER_SPECIAL_OPEN   5
+
+#define HOTLIST_DEFAULT_FILENAME "opera.adr"
+#define HOTLIST_FILTER "Hotlist Files\0opera.adr;opera5.adr;opera6.adr\0ADR Files\0*.adr\0"
+#define HOTLIST_NOT_FOUND "Your existing hotlist file could not be found.\n\n" \
+                            "Would you like to locate this file?\n"
+
+
+
+// The interface
+
+#ifndef COMPILING_RC
+int op_readFile(char *);
+int op_writeFile(char *);
+void BuildMenu(HMENU menu, CBookmarkNode *node, BOOL isContinuation);
+void BuildRebar(HWND hWndTB);
+int ParseHotlist(char **bmFileBuffer, CBookmarkNode &node);
+int SaveHotlistEntry(FILE *bmFile, CBookmarkNode *node);
+#endif
+
+void Config(HWND parent);
+
+
+#define _T(x) x
+#define PREFERENCE_REBAR_ENABLED _T("kmeleon.plugins.hotlist.rebar")
+#define PREFERENCE_HOTLIST_FILE  _T("kmeleon.plugins.hotlist.hotlistFile")
+#define PREFERENCE_HOTLIST_RESYNCH  _T("kmeleon.plugins.hotlist.resynch")
+#define PREFERENCE_SETTINGS_DIR    "kmeleon.general.settingsDir"
+
+
+// The globals
+
+#ifdef WHERE
+CBookmarkNode gHotlistRoot(0, "", "", BOOKMARK_FOLDER, 0);
+#else
+#define WHERE extern
+WHERE CBookmarkNode gHotlistRoot;
+#endif
+
+WHERE BOOL bRebarEnabled;
+WHERE BOOL bResynchHotlist;
+WHERE BOOL bCreate;
+WHERE CHAR gHotlistFile[MAX_PATH];
+
+extern kmeleonPlugin kPlugin;
+
+WHERE UINT nConfigCommand;
+WHERE UINT nAddCommand;
+WHERE UINT nAddLinkCommand;
+// WHERE UINT nAddToolbarCommand;
+// WHERE UINT nEditCommand;
+WHERE UINT nDropdownCommand;
+WHERE UINT nFirstHotlistPosition;
+
+WHERE char *lpszHotlistFile;
+
+#define HOTLIST_TITLE_LEN 64
+WHERE char gHotlistTitle[HOTLIST_TITLE_LEN];
+
+WHERE HMENU gMenuHotlist;
+
+WHERE BOOL gHotlistModified;
+
+WHERE int gMaxMenuLength;
+WHERE BOOL gMenuAutoDetect;
+
+WHERE BOOL bDOS;
+WHERE BOOL bEmpty;
+
+WHERE HWND ghWndTB;
+WHERE HIMAGELIST gImagelist; // the one and only imagelist...
+#endif
---
+++ k-meleon/op_hotlist/op_plugin.cpp
@@ -0,0 +1,386 @@
+/*
+ * Copyright (C) 2002 Ulf Erikson <ulferikson@fastmail.fm>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifdef __MINGW32__
+#  define _WIN32_IE 0x0500
+#  include <windows.h>
+#  include <commctrl.h>
+#  include "../missing.h"
+#endif
+
+#define KMELEON_PLUGIN_EXPORTS
+#include "kmeleon_plugin.h"
+#include "Utils.h"
+#include <errno.h>
+
+#define WHERE
+#include "op_hotlist.h"
+
+BOOL APIENTRY DllMain (
+   HANDLE hModule,
+   DWORD ul_reason_for_call,
+   LPVOID lpReserved) { 
+
+  return TRUE;
+}
+
+LRESULT CALLBACK WndProc (
+   HWND hWnd, UINT message, 
+   WPARAM wParam, 
+   LPARAM lParam);
+
+void * KMeleonWndProc;
+
+int Init();
+void Create(HWND parent);
+void Config(HWND parent);
+void Quit();
+void DoMenu(HMENU menu, char *param);
+long DoMessage(const char *to, const char *from, const char *subject, long data1, long data2);
+void DoRebar(HWND rebarWnd);
+int DoAccel(char *param);
+
+kmeleonPlugin kPlugin = {
+   KMEL_PLUGIN_VER,
+   "Opera Hotlist Plugin",
+   DoMessage
+};
+
+long DoMessage(const char *to, const char *from, const char *subject, long data1, long data2)
+{
+   if (to[0] == '*' || stricmp(to, kPlugin.dllname) == 0) {
+      if (stricmp(subject, "Init") == 0) {
+         Init();
+      }
+      else if (stricmp(subject, "Create") == 0) {
+         Create((HWND)data1);
+      }
+      else if (stricmp(subject, "Config") == 0) {
+         Config((HWND)data1);
+      }
+      else if (stricmp(subject, "Quit") == 0) {
+         Quit();
+      }
+      else if (stricmp(subject, "DoMenu") == 0) {
+         DoMenu((HMENU)data1, (char *)data2);
+      }
+      else if (stricmp(subject, "DoRebar") == 0) {
+         DoRebar((HWND)data1);
+      }
+      else if (stricmp(subject, "DoAccel") == 0) {
+         *(int *)data2 = DoAccel((char *)data1);
+      }
+      else return 0;
+      
+      return 1;
+   }
+   return 0;
+}
+
+BOOL BrowseForHotlist(char *file)
+{
+   OPENFILENAME ofn;
+   ofn.lStructSize = sizeof(OPENFILENAME);
+   ofn.hInstance = kPlugin.hDllInstance;
+   ofn.hwndOwner = NULL;
+   ofn.lpstrCustomFilter = NULL;
+   ofn.nMaxCustFilter = 0;
+   ofn.nFilterIndex = 1;
+   ofn.lpstrFileTitle = NULL;
+   ofn.nMaxFileTitle = MAX_PATH;
+   ofn.lpstrInitialDir = NULL;
+   ofn.nFileOffset = 0;
+   ofn.nFileExtension = 0;
+   ofn.lpstrDefExt = NULL;
+   ofn.lCustData = 0;
+   ofn.lpfnHook = NULL;
+   ofn.lpTemplateName = NULL;
+   ofn.lpstrFilter = HOTLIST_FILTER;
+   ofn.lpstrFile = file;
+   ofn.nMaxFile = MAX_PATH;
+   ofn.Flags = OFN_PATHMUSTEXIST |  OFN_LONGNAMES | OFN_EXPLORER | OFN_HIDEREADONLY;
+   ofn.lpstrTitle = PLUGIN_NAME;
+   
+   if (GetOpenFileName(&ofn)) {
+      return true;
+   }
+   else {
+      return false;
+   }
+}
+
+void getHotlistFile() {
+   FILE *bmFile;
+   char tmp[2048];
+   
+   kPlugin.kFuncs->GetPreference(PREF_STRING, PREFERENCE_HOTLIST_FILE, gHotlistFile, (char*)"");
+   
+   if (!gHotlistFile[0]) {
+      kPlugin.kFuncs->GetPreference(PREF_STRING, PREFERENCE_SETTINGS_DIR, gHotlistFile, (char*)"");
+      strcat(gHotlistFile, HOTLIST_DEFAULT_FILENAME);
+   }
+   
+   bmFile = fopen(gHotlistFile, "r");
+   while (!bmFile) {
+   retry:
+      kPlugin.kFuncs->GetPreference(PREF_STRING, PREFERENCE_SETTINGS_DIR, gHotlistFile, (char*)"");
+      strcat(gHotlistFile, HOTLIST_DEFAULT_FILENAME);
+      
+      strcpy(tmp, "Would you like to set your hotlist file to the default value?\n\n'");
+      strcat(tmp, gHotlistFile);
+      strcat(tmp, "'\n");
+      
+      while (!bmFile && 
+				 MessageBox(NULL, tmp, PLUGIN_NAME, 
+								MB_ICONQUESTION | MB_YESNO) == IDNO) {
+         if (!BrowseForHotlist(gHotlistFile)) {
+            goto retry;
+         }
+         bmFile = fopen(gHotlistFile, "r");
+         if (!bmFile && errno == ENOENT) {
+            strcpy(tmp, "File not found:\n'");
+            strcat(tmp, gHotlistFile);
+            strcat(tmp, "'\n\nCreate it?\n");
+         }
+      }
+      
+      if (!bmFile) {
+         bmFile = fopen(gHotlistFile, "wb");
+         if (bmFile) {
+            fprintf(bmFile, 
+                    "Opera Hotlist version 2.0\r\n"
+                    "Options:encoding=utf8,version=3\r\n\r\n");
+            bDOS = 1;
+            strcpy(tmp, "Hotlist file '");
+            strcat(tmp, gHotlistFile);
+            strcat(tmp, "' created.\n");
+            MessageBox(NULL, tmp, PLUGIN_NAME, MB_OK);
+         }
+         else {
+            strcpy(tmp, "Unable to create file '");
+            strcat(tmp, gHotlistFile);
+            strcat(tmp, "'.\n");
+            MessageBox(NULL, tmp, PLUGIN_NAME, MB_ICONEXCLAMATION | MB_OK);
+         }
+      }
+   }
+   if (bmFile)
+      fclose(bmFile);
+   
+   kPlugin.kFuncs->SetPreference(PREF_STRING, PREFERENCE_HOTLIST_FILE, gHotlistFile);
+}
+
+int Init(){
+   nConfigCommand = kPlugin.kFuncs->GetCommandIDs(1);
+   nAddCommand = kPlugin.kFuncs->GetCommandIDs(1);
+   nAddLinkCommand = kPlugin.kFuncs->GetCommandIDs(1);
+   
+   nDropdownCommand = kPlugin.kFuncs->GetCommandIDs(1);
+   kPlugin.kFuncs->GetPreference(PREF_BOOL, PREFERENCE_REBAR_ENABLED, &bRebarEnabled, &bRebarEnabled);
+   kPlugin.kFuncs->GetPreference(PREF_BOOL, PREFERENCE_HOTLIST_RESYNCH, &bResynchHotlist, &bResynchHotlist);
+   
+   getHotlistFile();
+   bEmpty = true;
+   
+   gImagelist = ImageList_Create(16, 15, ILC_MASK, 4, 4);
+   HBITMAP bitmap = LoadBitmap(kPlugin.hDllInstance, MAKEINTRESOURCE(IDB_IMAGES));
+   ImageList_AddMasked(gImagelist, bitmap, RGB(192, 192, 192));
+   DeleteObject(bitmap);
+   
+   return true;
+}
+
+void Create(HWND parent){
+   KMeleonWndProc = (void *) GetWindowLong(parent, GWL_WNDPROC);
+   SetWindowLong(parent, GWL_WNDPROC, (LONG)WndProc);
+}
+
+// Preferences Dialog function
+BOOL CALLBACK DlgProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+   
+   switch (uMsg) {
+      case WM_INITDIALOG:
+         SendDlgItemMessage(hWnd, IDC_REBARENABLED, BM_SETCHECK, bRebarEnabled, 0);
+         break;
+      case WM_COMMAND:
+         switch(HIWORD(wParam)) {
+            case BN_CLICKED:
+               switch (LOWORD(wParam)) {
+                  case IDOK:
+                     bRebarEnabled = SendDlgItemMessage(hWnd, IDC_REBARENABLED, BM_GETCHECK, 0, 0);
+                     kPlugin.kFuncs->SetPreference(PREF_BOOL, PREFERENCE_REBAR_ENABLED, &bRebarEnabled);
+                  case IDCANCEL:
+                     SendMessage(hWnd, WM_CLOSE, 0, 0);
+               }
+         }
+         break;
+      case WM_CLOSE:
+         EndDialog(hWnd, 0);
+         break;
+      default:
+         return FALSE;
+   }
+   return TRUE;
+}
+
+void Config(HWND hWndParent){
+   DialogBoxParam(kPlugin.hDllInstance ,MAKEINTRESOURCE(IDD_CONFIG), hWndParent, (DLGPROC)DlgProc, 0);
+}
+
+void Quit(){
+   ImageList_Destroy(gImagelist);
+}
+
+void DoMenu(HMENU menu, char *param){
+   if (*param != 0){
+      char *action = param;
+      char *string = strchr(param, ',');
+      if (string) {
+         *string = 0;
+         string = SkipWhiteSpace(string+1);
+      }
+      else
+         string = action;
+      
+      int command = 0;
+      if (stricmp(action, "Add") == 0){
+         command = nAddCommand;
+      }
+      if (stricmp(action, "AddLink") == 0){
+         command = nAddLinkCommand;
+      }
+      if (stricmp(param, "Config") == 0)
+         command = nConfigCommand;
+      if (command) {
+         AppendMenu(menu, MF_STRING, command, string);
+         return;
+      }
+      return;
+   }
+   
+   gMenuHotlist = menu;
+   nFirstHotlistPosition = GetMenuItemCount(menu);
+   
+   int ret = -1;
+   lpszHotlistFile = strdup(gHotlistFile);
+   if (lpszHotlistFile && *lpszHotlistFile) {
+      if ((ret = op_readFile(lpszHotlistFile)) > 0) {
+         BuildMenu(menu, &gHotlistRoot, false);
+      }
+   }
+   
+   if (ret < 0) {
+      if (!lpszHotlistFile || !*lpszHotlistFile)
+         MessageBox(NULL, 
+                    "No hotlist specified.",
+                    "Hotlist Error", MB_ICONSTOP|MB_OK);
+      else {
+         if (!bCreate || errno != ENOENT) {
+            char tmp[2048];
+            strcpy(tmp, "Unable to open hotlist file\r\n'");
+            strcat(tmp, lpszHotlistFile);
+            strcat(tmp, "'.");
+            if (errno == ENOENT) {
+               strcat(tmp, "\r\nFile not found.");
+            }
+            MessageBox(NULL, tmp, "Hotlist Error", MB_ICONSTOP|MB_OK);
+         }
+      }
+   }
+}
+
+int DoAccel(char *param) {
+   if (stricmp(param, "Config") == 0)
+      return nConfigCommand;
+   return 0;
+}
+
+void DoRebar(HWND rebarWnd) {
+   
+   // disabled to fix "create new window pauses for several seconds" bug
+   if (bRebarEnabled) {
+      
+      DWORD dwStyle = 0x40 | /*the 40 gets rid of an ugly border on top.  I have no idea what flag it corresponds to...*/
+         CCS_NOPARENTALIGN | CCS_NORESIZE | //CCS_ADJUSTABLE |
+         TBSTYLE_FLAT | TBSTYLE_TRANSPARENT | TBSTYLE_LIST | TBSTYLE_TOOLTIPS;
+      
+      // Create the toolbar control to be added.
+      ghWndTB = CreateWindowEx(0, TOOLBARCLASSNAME, "",
+                               WS_CHILD | dwStyle,
+                               0,0,0,0,
+                               rebarWnd, (HMENU)/*id*/200,
+                               kPlugin.hDllInstance, NULL
+                               );
+      
+      
+      // Register the band name and child hwnd
+      kPlugin.kFuncs->RegisterBand(ghWndTB, TOOLBAND_NAME);
+      
+      if (!ghWndTB){
+         MessageBox(NULL, TOOLBAND_FAILED_TO_CREATE, NULL, 0);
+         return;
+      }
+      
+      BuildRebar(ghWndTB);
+      
+      // Get the height of the toolbar.
+      DWORD dwBtnSize = SendMessage(ghWndTB, TB_GETBUTTONSIZE, 0,0);
+      
+      REBARBANDINFO rbBand;
+      rbBand.cbSize = sizeof(REBARBANDINFO);  // Required
+      rbBand.fMask  = //RBBIM_TEXT |
+         RBBIM_STYLE | RBBIM_CHILD  | RBBIM_CHILDSIZE |
+         RBBIM_SIZE | RBBIM_IDEALSIZE;
+      
+      rbBand.fStyle = RBBS_CHILDEDGE | RBBS_FIXEDBMP | RBBS_VARIABLEHEIGHT;
+      rbBand.lpText     = PLUGIN_NAME;
+      rbBand.hwndChild  = ghWndTB;
+      rbBand.cxMinChild = 0;
+      rbBand.cyMinChild = HIWORD(dwBtnSize);
+      rbBand.cyIntegral = 1;
+      rbBand.cyMaxChild = rbBand.cyMinChild;
+      rbBand.cxIdeal    = 0;
+      rbBand.cx         = rbBand.cxIdeal;
+      
+      // Add the band that has the toolbar.
+      SendMessage(rebarWnd, RB_INSERTBAND, (WPARAM)-1, (LPARAM)&rbBand);
+      
+   }
+}
+
+extern "C" {
+   KMELEON_PLUGIN kmeleonPlugin *GetKmeleonPlugin() {
+      return &kPlugin;
+   }
+   
+   KMELEON_PLUGIN int DrawBitmap(DRAWITEMSTRUCT *dis) {
+      int top = (dis->rcItem.bottom - dis->rcItem.top - 16) / 2;
+      top += dis->rcItem.top;
+      
+      if (GetMenuState((HMENU)dis->hwndItem, dis->itemID, 0) & MF_POPUP){
+         if (dis->itemState & ODS_SELECTED){
+            ImageList_Draw(gImagelist, IMAGE_FOLDER_OPEN, dis->hDC, dis->rcItem.left, top, ILD_TRANSPARENT | ILD_FOCUS );
+         }
+         else{
+            ImageList_Draw(gImagelist, IMAGE_FOLDER_CLOSED, dis->hDC, dis->rcItem.left, top, ILD_TRANSPARENT);
+         }
+         return 18;
+      }
+      return 0;
+   }
+}
