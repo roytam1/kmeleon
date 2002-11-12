bc28ca4d551d6697f23345fb2d615327c7a48193
ulferikson <ulferikson@localhost>
2002-11-12 18:52:20 +0000

    Feature: AddLink for context popup menus and macros 'pluginmsg' command.

--- k-meleon/op_hotlist/op_plugin.cpp
+++ k-meleon/op_hotlist/op_plugin.cpp
@@ -85,6 +85,9 @@ long DoMessage(const char *to, const char *from, const char *subject, long data1
       else if (stricmp(subject, "DoAccel") == 0) {
          *(int *)data2 = DoAccel((char *)data1);
       }
+      else if (stricmp(subject, "AddLink") == 0) {
+         addLink((char *)data1, (char *)data2);
+      }
       else return 0;
       
       return 1;
