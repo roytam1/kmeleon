12bc515fb588f7772c051a15d58e63b9d5882a62
ulferikson <ulferikson@localhost>
2002-12-08 11:26:27 +0000

    Fixes:   Bug #255 - Problem with printer image on toolbar graphic.   Correcting the default UA (http://www.geocrawler.com/archives/3/9632/2002/11/0/10172689/).   Add Alt-Home to open home page to match Mozilla.   "Better" up_directory macro.

--- k-meleon/default/profile/prefs.js
+++ k-meleon/default/profile/prefs.js
@@ -1,7 +1,8 @@
 # Mozilla User Preferences
 // This is a generated file!
 
-user_pref("general.useragent.vendor", "K-Meleon");
+user_pref("browser.xul.error_pages.enabled", true);
+user_pref("general.useragent.vendor", "K-Meleon/0.7");
 user_pref("kmeleon.plugins.bmpmenu.load", true);
 user_pref("kmeleon.plugins.bookmarks.load", true);
 user_pref("kmeleon.plugins.external.load", true);
@@ -27,13 +28,13 @@ user_pref("kmeleon.privacy.useragent4.string", "Mozilla/4.0 (compatible; MSIE 5.
 user_pref("kmeleon.privacy.useragent5.name", "MSIE 4.0");
 user_pref("kmeleon.privacy.useragent5.string", "Mozilla/4.0 (compatible; MSIE 4.0; Windows NT)");
 user_pref("kmeleon.toolband.Bookmarks.break", 1);
-user_pref("kmeleon.toolband.Bookmarks.index", 4);
+user_pref("kmeleon.toolband.Bookmarks.index", 3);
 user_pref("kmeleon.toolband.Bookmarks.size", 0);
 user_pref("kmeleon.toolband.Bookmarks.visibility", true);
-user_pref("kmeleon.toolband.Favorites.break", 1);
-user_pref("kmeleon.toolband.Favorites.index", 5);
-user_pref("kmeleon.toolband.Favorites.size", 0);
-user_pref("kmeleon.toolband.Favorites.visibility", true);
+user_pref("kmeleon.toolband.Layers.break", 1);
+user_pref("kmeleon.toolband.Layers.index", 4);
+user_pref("kmeleon.toolband.Layers.size", 771);
+user_pref("kmeleon.toolband.Layers.visibility", true);
 user_pref("kmeleon.toolband.Menu.break", 0);
 user_pref("kmeleon.toolband.Menu.index", 0);
 user_pref("kmeleon.toolband.Menu.size", 251);
@@ -46,7 +47,5 @@ user_pref("kmeleon.toolband.URL Bar.break", 0);
 user_pref("kmeleon.toolband.URL Bar.index", 2);
 user_pref("kmeleon.toolband.URL Bar.size", 866);
 user_pref("kmeleon.toolband.URL Bar.visibility", true);
-user_pref("kmeleon.toolband.layers.break", 1);
-user_pref("kmeleon.toolband.layers.index", 4);
-user_pref("kmeleon.toolband.layers.size", 771);
+user_pref("layout.xml.prettyprint", true);
 user_pref("network.dir.generate_html", true);
