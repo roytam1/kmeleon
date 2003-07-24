cd88111c4eeea9d72d482746e2d37d5431d101ef
ulferikson <ulferikson@localhost>
2003-07-24 13:34:59 +0000

    Bug 449: File download without content-length header kicks counters into negative numbers Removing also file size when the percentage is bad. Only what's downloaded is shown now.

--- k-meleon/UnknownContentTypeHandler.cpp
+++ k-meleon/UnknownContentTypeHandler.cpp
@@ -439,9 +439,8 @@ NS_IMETHODIMP CProgressDialog::OnProgressChange(nsIWebProgress *aWebProgress, ns
       if (percent>=0 && percent<=100)
         progressString.Format(IDS_DOWNLOAD_PROGRESS, percent, ((double)aCurTotalProgress)/1024, ((double)aMaxTotalProgress)/1024);
       else
-        progressString.Format(IDS_DOWNLOAD_PROGRESS2, ((double)aCurTotalProgress)/1024, ((double)aMaxTotalProgress)/1024);
+        progressString.Format(IDS_DOWNLOAD_PROGRESS2, ((double)aCurTotalProgress)/1024);
       SetDlgItemText(IDC_STATUS, progressString);
-      
 
       PRInt64 now = PR_Now();
       PRInt64 timeSpent = now - mStartTime;
