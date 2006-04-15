fe80dd6c2e2a831580cd3f5df1e3739e9eeb7b7c
boisso <boisso@localhost>
2006-04-15 15:04:27 +0000

    GetKmeleonVersion

--- k-meleon/KMeleonConst.h
+++ k-meleon/KMeleonConst.h
@@ -16,6 +16,8 @@
 *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+#define KMELEON_VERSION				   0x00010000
+
 #define WM_DEFERSHOW				   WM_USER + 150
 #define UWM_UPDATESESSIONHISTORY       WM_APP + 110
 #define UWM_REFRESHTOOLBARITEM         WM_APP + 111
