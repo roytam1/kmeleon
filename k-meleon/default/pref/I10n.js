cc5dab7edfe782c46f10465c5b3555f5f7972ce5
boisso <boisso@localhost>
2006-04-15 09:17:11 +0000

    Default pref for 1.0

---
+++ k-meleon/default/pref/I10n.js
@@ -0,0 +1,5 @@
+
+//pref("general.useragent.locale", "en-US");
+//pref("intl.charset.default", "ISO-8859-1");
+//pref("intl.accept_languages", "en-us, en");
+//pref("kmeleon.general.translation", "en");
