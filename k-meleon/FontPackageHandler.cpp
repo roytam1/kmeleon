32882d475541cce39228c2fb5281eb1776c62f6f
boisso <boisso@localhost>
2006-04-29 20:00:32 +0000

    Add a pref to never show the dialog

--- k-meleon/FontPackageHandler.cpp
+++ k-meleon/FontPackageHandler.cpp
@@ -1,5 +1,24 @@
+/*
+*  Copyright (C) 2005 Dorian Boissonnade
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2, or (at your option)
+*  any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*
+*/
+
 #include "stdafx.h"
-#include ".\fontpackagehandler.h"
+#include "Fontpackagehandler.h"
 #include "nsIFontPackageService.h"
 #include "BrowserFrm.h"
 #include "MfcEmbed.h"
@@ -22,6 +41,10 @@ NS_IMETHODIMP CFontPackageHandler::NeedFontPackage(const char *aFontPackID)
 
   if (!strlen(aFontPackID))
     return NS_ERROR_UNEXPECTED;
+
+  CMfcEmbedApp *pApp = (CMfcEmbedApp *)AfxGetApp();
+  if (!pApp->preferences.GetBool("font.askWhenNeeded", true))
+	  return NS_OK;
 
   nsresult rv;
 
