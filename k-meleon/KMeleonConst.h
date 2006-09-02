756f6df9e60125b7570c81d3ce199ecd2c35ac60
boisso <boisso@localhost>
2006-09-02 10:27:50 +0000

    Update version number

--- k-meleon/KMeleonConst.h
+++ k-meleon/KMeleonConst.h
@@ -16,7 +16,7 @@
 *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#define KMELEON_VERSION				   0x01000201
+#define KMELEON_VERSION				   0x01000203
 
 #define WM_DEFERSHOW				   WM_USER + 150
 #define WM_DEFERSAVEAS                 WM_USER + 151
