0d7c1e4590245b35c99aec62dddfaef55bb88f50
boisso <boisso@localhost>
2006-12-10 10:56:50 +0000

    Be nice with old system?

--- k-meleon/rebar_menu/StdAfx.h
+++ k-meleon/rebar_menu/StdAfx.h
@@ -2,6 +2,7 @@
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
 //
+#define  WINVER  0x0400
 
 #if !defined(AFX_STDAFX_H__1EFDBDD3_12F9_11D5_AA2A_00E02916DA72__INCLUDED_)
 #define AFX_STDAFX_H__1EFDBDD3_12F9_11D5_AA2A_00E02916DA72__INCLUDED_
