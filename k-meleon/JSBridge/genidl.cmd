867f1e96c54037505d5010dbb155c6975c1384dd
boisso <boisso@localhost>
2007-10-20 17:28:37 +0000

    no message

--- k-meleon/JSBridge/genidl.cmd
+++ k-meleon/JSBridge/genidl.cmd
@@ -1,2 +1,2 @@
 @..\..\mozilla\mozilla\dist\bin\xpidl -m header -I ..\..\mozilla\mozilla\dist\idl nsIJSBridge.idl 
-@..\..\mozilla\mozilla\dist\bin\xpidl -e components\js_bridge.xpt -m typelib -I ..\..\mozilla\mozilla\dist\idl nsIJSBridge.idl 
+@..\..\mozilla\mozilla\dist\bin\xpidl -e js_bridge.xpt -m typelib -I ..\..\mozilla\mozilla\dist\idl nsIJSBridge.idl 
