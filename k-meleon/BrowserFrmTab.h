3a03e26df193d4150a11ac3bd843d7ab99692e18
boisso <boisso@localhost>
2007-11-16 21:24:17 +0000

    Fix keyword search when opening a new tab

--- k-meleon/BrowserFrmTab.h
+++ k-meleon/BrowserFrmTab.h
@@ -50,8 +50,8 @@ public:
 	inline bool IsActive() {return m_bActive;}
 	inline bool SetActive(bool state, bool haveFocus=true);
 
-	CBrowserTab*   OpenURLInNewTab(LPCTSTR url, LPCTSTR refferer = NULL, BOOL bBackground=FALSE);
-	CBrowserFrame* OpenURLInNewWindow(LPCTSTR url, LPCTSTR refferer = NULL, BOOL bBackground=FALSE);
+	CBrowserTab*   OpenURLInNewTab(LPCTSTR url, LPCTSTR refferer = NULL, BOOL bBackground = FALSE, BOOL allowFixup = FALSE);
+	CBrowserFrame* OpenURLInNewWindow(LPCTSTR url, LPCTSTR refferer = NULL, BOOL bBackground = FALSE);
 	virtual void OpenURLWithCommand(UINT idCommand, LPCTSTR url, LPCTSTR refferer = NULL, BOOL allowFixup = FALSE);
 /*	void SetFrameGlue(PBROWSERFRAMEGLUE glue){
 		m_wndView.SetBrowserFrameGlue(glue);
