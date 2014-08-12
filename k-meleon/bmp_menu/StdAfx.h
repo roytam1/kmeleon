31de25009913ba81ffa5bd518763aa703f7e132a
boisso <boisso@localhost>
2014-08-12 18:33:47 +0000

    Fix unicode characters in menus

--- k-meleon/bmp_menu/StdAfx.h
+++ k-meleon/bmp_menu/StdAfx.h
@@ -14,7 +14,8 @@
 
 // Insert your headers here
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-
+#define _UNICODE 1
+#define UNICODE 1
 #include <windows.h>
 
 #include <commctrl.h>
