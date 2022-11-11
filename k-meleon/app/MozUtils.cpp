5df6dc285050ee750ef0423a8d97633f9231c0fa
roytam1 <roytam@gmail.com>
2022-11-11 10:54:29 +0800

    MozUtils: update NotifyJSRunToCompletionStart() API (as of NM27 rev f468c29a)

--- k-meleon/app/MozUtils.cpp
+++ k-meleon/app/MozUtils.cpp
@@ -739,8 +739,10 @@ bool InjectJS(nsIDOMWindow* dom, const wchar_t* userScript, CString& result)
 	NS_ENSURE_SUCCESS(cs->Push(cx), FALSE);
 	JSAutoRequest ar(cx);
 	JSAutoNullableCompartment ac(cx, _globalJSObject);
+	JS::Rooted<JS::Value> asyncStack(cx, JS::NullValue());
+	JS::Rooted<JS::Value> asyncCauseValue(cx, JS::NullValue());
 
-	docShell->NotifyJSRunToCompletionStart("InjectJS", L"InjectJS", L"InjectJS", 1);
+	docShell->NotifyJSRunToCompletionStart("InjectJS", L"InjectJS", L"InjectJS", 1, asyncStack, asyncCauseValue);
 	
 	JS::Rooted<JSObject*> globalJSObject(cx, innerGlobal->GetGlobalJSObject());
 	JS::Rooted<JS::Value> v (cx, JS::UndefinedValue());
