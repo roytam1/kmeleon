b81834edaea9de60094195bd1a6230f8c7a2652e
roytam1 <roytam@gmail.com>
2022-12-14 16:40:45 +0800

    IDL build fix

--- k-meleon/JSBridge/genidl.cmd
+++ k-meleon/JSBridge/genidl.cmd
@@ -1,2 +1,2 @@
-@..\..\xulrunner-sdk\bin\header.py -I ../../xulrunner-sdk/idl -o nsIJSBridge.h nsIJSBridge.idl 
-@..\..\xulrunner-sdk\bin\typelib.py -I ../../xulrunner-sdk/idl  -o jsbridge.xpt nsIJSBridge.idl 
+@c:\python27\python ..\..\xulrunner-sdk\sdk\bin\header.py -I ../../xulrunner-sdk/idl -o nsIJSBridge.h nsIJSBridge.idl 
+@c:\python27\python ..\..\xulrunner-sdk\sdk\bin\typelib.py -I ../../xulrunner-sdk/idl  -o jsbridge.xpt nsIJSBridge.idl 
--- k-meleon/login/genidl.cmd
+++ k-meleon/login/genidl.cmd
@@ -1,2 +1,2 @@
-@..\..\xulrunner-sdk\bin\header.py -I ../../xulrunner-sdk/idl -o kmIHelper.h kmHelper.idl 
-@..\..\xulrunner-sdk\bin\typelib.py -I ../../xulrunner-sdk/idl  -o kmHelper.xpt kmHelper.idl 
+@c:\python27\python ..\..\xulrunner-sdk\sdk\bin\header.py -I ../../xulrunner-sdk/idl -o kmIHelper.h kmHelper.idl 
+@c:\python27\python ..\..\xulrunner-sdk\sdk\bin\typelib.py -I ../../xulrunner-sdk/idl  -o kmHelper.xpt kmHelper.idl 
